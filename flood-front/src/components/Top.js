63c3961374b84554e3f0aa3f9f64da8b2126cf18
Elena Busila <elena.busila@gmail.com>
2019-04-29 15:20:34 -0400

    Dockerize webapp
    Added local setup in the README
    
    Small linting fixes
    
    Use env. variable for google api key

--- flood-front/src/components/Top.js
+++ flood-front/src/components/Top.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import { Menu, Row, Col, Typography } from 'antd';
+import React, {Component} from 'react';
+import {Menu, Row, Col, Typography} from 'antd';
 
-const { Title } = Typography;
+const {Title} = Typography;
 
 class Top extends Component {
     render() {
         return (
-            <Row gutter={ 32 } type="flex" justify="space-between" align="middle"
-                style={ {
-                    boxShadow: "10px 1px 8px rgba(0, 0, 0, .5)",
-                    minHeight: 80,
-                    backgroundColor: "white"
-                } }
+            <Row gutter={32} type="flex" justify="space-between" align="middle"
+                 style={{
+                     boxShadow: "10px 1px 8px rgba(0, 0, 0, .5)",
+                     minHeight: 80,
+                     backgroundColor: "white"
+                 }}
             >
-                <Col span={ 12 } style={{padding: 20}}>
-                    <Title level={ 1 } style={ {
+                <Col span={12} style={{padding: 20}}>
+                    <Title level={1} style={{
                         textAlign: 'center',
                         fontSize: '2rem',
                         fontWeight: 400,
                         marginBottom: 0
-                    } }>Climate Change AI</Title>
+                    }}>Climate Change AI</Title>
                 </Col>
-                <Col span={ 12 }>
+                <Col span={12}>
                     <Menu
                         mode="horizontal"
-                        style={ {
+                        style={{
                             display: 'flex',
                             justifyContent: 'space-evenly',
                             border: "none"
-                        } }
+                        }}
                     >
-                        <Menu.Item style={ {
+                        <Menu.Item style={{
                             fontSize: '1.2rem'
-                        } } key="1">Reasearch</Menu.Item>
-                        <Menu.Item style={ {
+                        }} key="1">Reasearch</Menu.Item>
+                        <Menu.Item style={{
                             fontSize: '1.2rem'
-                        } } key="2">About</Menu.Item>
+                        }} key="2">About</Menu.Item>
                     </Menu>
                 </Col>
             </Row>
