939ce752c88e607da9a0ac6ae3a47b4947883a3d
notoraptor <stevenbocco@gmail.com>
2019-07-22 13:50:57 -0400

    Add image comparison slider.

--- reactapp/src/pages/ganifyResult.js
+++ reactapp/src/pages/ganifyResult.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import {Act} from "./act";
 import {StaticPage} from "../components/StaticPage";
 import {AddressManager} from "../components/AddressManager";
+import ReactCompareImage from 'react-compare-image';
 
 export class GanifyResult extends React.Component {
 	render() {
@@ -16,12 +17,19 @@ export class GanifyResult extends React.Component {
 						<AddressManager showMap={true} initialAddress={this.props.address}/>
 					</div>
 					<div className="col-md-6 text-center">
-						<div>
-							<img className="img-fluid"
-								 id="ganify-image"
-								 alt={address ? `scenario render for address: ${address}` : `No address to show`}
-								 src={this.props.image}/>
-						</div>
+						{address ? (
+							<ReactCompareImage leftImage={this.props.image}
+											   key={address}
+											   rightImage={this.props.image}
+											   leftImageAlt={'Original image'}
+											   rightImageAlt={'Modified image'}
+											   rightImageCss={{
+												   "-webkit-filter": "grayscale(100%)",
+												   filter: "grayscale(100%)"
+											   }}/>
+						) : (
+							<div className="no-image">No address to show</div>
+						)}
 						<div className="my-4">
 							<button className="btn btn-secondary btn-lg px-5" onClick={() => pageLoader('act', <Act/>)}>
 								<strong>Make it change : have an impact</strong>
