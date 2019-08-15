38673c650d2f3c4567016644f929442af1565631
notoraptor <stevenbocco@gmail.com>
2019-08-15 12:29:17 -0400

    Integrate new API.

--- reactapp/src/api/ganify.js
+++ reactapp/src/api/ganify.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 export function ganify(address, onresult, onerror) {
 	// const addressForUrl = stringToBase64(address);
-	const addressForUrl = address;
+	const addressForUrl = encodeURI(address);
 
 	// Remote end point.
-	const endPoint = 'https://api.climatechangeai.org/address/munit';
+	// const endPoint = 'https://api.climatechangeai.org/address/munit';
+	const endPoint = 'https://api.climatechangeai.org/flood/munit';
 
 	// Local end point.
 	// const endPoint = 'http://localhost:5000/address/01';
@@ -21,11 +22,11 @@ export function ganify(address, onresult, onerror) {
 		.then(reponse => {
 			// Is this possible?
 			if (!reponse)
-				throw new Error(`No image available for address ${address}`);
+				throw new Error(`No images for address ${address}`);
 			const reader = new FileReader();
 			reader.readAsDataURL(reponse.data);
 			reader.onloadend = function () {
-				console.log(`Image loaded for ${address}`);
+				console.log(`Images loaded for ${address}`);
 				onresult(reader.result);
 			};
 		})
@@ -33,4 +34,13 @@ export function ganify(address, onresult, onerror) {
 			if (onerror)
 				onerror(error);
 		});
+}
+
+export function promiseGanify(address) {
+	return new Promise((resolve, reject) => {
+		if (address)
+			ganify(address, resolve, reject);
+		else
+			resolve(null);
+	})
 }
--- reactapp/src/pages/ganifyResult.js
+++ reactapp/src/pages/ganifyResult.js
@@ -7,29 +7,70 @@ import {AddressManager} from "../components/AddressManager";
 import ReactCompareImage from 'react-compare-image';
 
 export class GanifyResult extends React.Component {
+	constructor(props) {
+		super(props);
+		const address = props.initialAddress || '';
+		const result = props.initialResult || {};
+		this.state = {
+			address: address,
+			image: result.original ? `data:image/jpeg;base64,${result.original}` : null,
+			flooded: (result.flooded || result.original) ? `data:image/jpeg;base64,${result.flooded || result.original}` : null,
+			metadata: result.metadata,
+			warning: result.warning,
+			error: result.error
+		};
+		this.onSubmitted = this.onSubmitted.bind(this);
+	}
+	setState(state) {
+		return new Promise(resolve => super.setState(state, resolve));
+	}
+	onSubmitted(address, result) {
+		if (!result)
+			return false;
+		const header = 'data:application/json;base64,';
+		result = JSON.parse(atob(result.substr(header.length)));
+		return this.setState({
+			address: address,
+			image: result.original ? `data:image/jpeg;base64,${result.original}` : null,
+			flooded: (result.flooded || result.original) ? `data:image/jpeg;base64,${result.flooded || result.original}` : null,
+			metadata: result.metadata,
+			warning: result.warning,
+			error: result.error
+		});
+	}
 	render() {
-		const address = this.props.address;
+		const address = this.state.address;
 		const pageLoader = this.context.pageLoader;
+		const metadata = this.state.metadata ? Object.values(this.state.metadata) : null;
+		if (metadata)
+			metadata.sort((m1, m2) => m1.title.localeCompare(m2.title));
 		return (
 			<StaticPage name={'ganify'} title={address ? `${address}` : `Scenario`}>
 				<div className="row">
 					<div className="col-md-6">
-						<AddressManager showMap={true} initialAddress={this.props.address}/>
+						<AddressManager showMap={true}
+										initialAddress={this.state.address}
+										onSubmitted={this.onSubmitted}/>
 					</div>
 					<div className="col-md-6 text-center">
-						{address ? (
-							<ReactCompareImage leftImage={this.props.image}
-											   key={address}
-											   rightImage={this.props.image}
+						{this.state.image ? (
+							<ReactCompareImage leftImage={this.state.image}
+											   rightImage={this.state.flooded}
 											   leftImageAlt={'Original image'}
-											   rightImageAlt={'Modified image'}
-											   rightImageCss={{
-												   "-webkit-filter": "grayscale(100%)",
-												   filter: "grayscale(100%)"
-											   }}/>
+											   rightImageAlt={'Modified image'}/>
 						) : (
-							<div className="no-image">No address to show</div>
+							<div className="no-image">Nothing to show</div>
 						)}
+						{this.state.warning ? <div className="warning-message pt-4 px-2">{this.state.warning}</div> : ''}
+						{this.state.error ? <div className="error-message pt-4 px-2">{this.state.error}</div> : ''}
+						{metadata ? (
+							<div className="my-4 metadata">{metadata.map((entry, i) => (
+								<div className="row" key={i}>
+									<div className="col-md-8 metadata-title">{entry.title}</div>
+									<div className="col-md-4 metadata-value">{entry.value || 'N/A'}</div>
+								</div>
+							))}</div>
+						) : ''}
 						<div className="my-4">
 							<button className="btn btn-secondary btn-lg px-5" onClick={() => pageLoader('act', <Act/>)}>
 								<strong>Make it change : have an impact</strong>
@@ -43,7 +84,7 @@ export class GanifyResult extends React.Component {
 }
 
 GanifyResult.propTypes = {
-	address: PropTypes.string.isRequired,
-	image: PropTypes.string.isRequired
+	initialAddress: PropTypes.string.isRequired,
+	initialResult: PropTypes.object
 };
 GanifyResult.contextType = AppContext;
